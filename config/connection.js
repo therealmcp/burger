2f969272c57264ebc3669011a065355825fc682a
unknown <cp.mcpike@gmail.com>
2018-11-21 00:39:40 -0800

    heroku database code added

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,23 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "KraMerica123",
-    database: "burgers_db"
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+    // development: {
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "KraMerica123",
+        database: "burgers_db",
+        // dialect: "mysql"
+    // },
+    // production: {
+    //     use_env_variable: "JAWSDB_URL",
+    //     dialect: "mysql"
+    // }
 });
 
 connection.connect(function(err) {
